ccb8ce14dc7c7f221e73e5aabc0db0e072647c17
Schlickeiser <FIXCNJT@vw.vwgroup.com>
2026-04-02 14:10:53 +0200

    unique solution enabled

--- starter/sudoku_logic.py
+++ starter/sudoku_logic.py
@@ -48,10 +48,47 @@ def remove_cells(board, clues):
             board[row][col] = EMPTY
             attempts -= 1
 
+def count_solutions(board, limit=2):
+    """Count the number of solutions for the puzzle. Stop at limit to avoid long computation."""
+    def solve(pos):
+        if pos == SIZE * SIZE:
+            return 1
+        row, col = divmod(pos, SIZE)
+        if board[row][col] != EMPTY:
+            return solve(pos + 1)
+        count = 0
+        for num in range(1, SIZE + 1):
+            if is_safe(board, row, col, num):
+                board[row][col] = num
+                count += solve(pos + 1)
+                board[row][col] = EMPTY
+                if count >= limit:
+                    break
+        return count
+    
+    return solve(0)
+
 def generate_puzzle(clues=35):
     board = create_empty_board()
     fill_board(board)
     solution = deep_copy(board)
-    remove_cells(board, clues)
+    
+    # Create puzzle by removing cells while ensuring unique solution
+    cells_to_remove = SIZE * SIZE - clues
+    removed = 0
+    while removed < cells_to_remove:
+        # Find a filled cell to try removing
+        candidates = [(i, j) for i in range(SIZE) for j in range(SIZE) if board[i][j] != EMPTY]
+        if not candidates:
+            break
+        row, col = random.choice(candidates)
+        temp = board[row][col]
+        board[row][col] = EMPTY
+        if count_solutions(deep_copy(board)) == 1:
+            removed += 1
+        else:
+            # Put it back if removing it creates multiple solutions
+            board[row][col] = temp
+    
     puzzle = deep_copy(board)
     return puzzle, solution
